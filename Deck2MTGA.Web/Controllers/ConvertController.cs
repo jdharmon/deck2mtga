73812024c530654f595e3c159c293c2648810893
Jason Harmon <jason@jdharmon.com>
2018-04-23 16:13:23 -0400

    Return array for Accept: application/json

--- Deck2MTGA.Web/Controllers/ConvertController.cs
+++ Deck2MTGA.Web/Controllers/ConvertController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Deck2MTGA.Web.Models;
 using Deck2MTGA.Web.Repositories;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace Deck2MTGA.Web
 {
@@ -21,21 +23,43 @@ namespace Deck2MTGA.Web
 
         // POST api/convert
         [HttpPost]
-        public string Post([FromBody]string input)
+        [Consumes("application/json")]
+        [Produces("application/json", "text/plain")]
+        [SwaggerResponse((int)HttpStatusCode.OK)]
+        public IActionResult Post([FromBody]string input)
         {
+            if (input == null)
+                return BadRequest();
+
             var deck = new Deck(_cardRepository);
             deck.Parse(input);
-            return deck.ToArenaString();
+            var result = deck.ToArenaString();
+
+            if (Request.Headers["Accept"] == "text/plain")
+                return Ok(result);
+            else
+                return Ok(result.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries));
         }
 
         [HttpPost("Raw")]
-        public string PostRaw() 
+        [Consumes("text/plain")]
+        [Produces("text/plain")]
+        [SwaggerResponse((int)HttpStatusCode.OK, typeof(string))]
+        public IActionResult PostRaw() 
         {
             string input;
             using (var reader = new StreamReader(Request.Body)) {
                 input = reader.ReadToEnd();
             }
-            return Post(input);
+
+            if (string.IsNullOrEmpty(input))
+                return BadRequest();
+
+            var deck = new Deck(_cardRepository);
+            deck.Parse(input);
+            var result = deck.ToArenaString();
+
+            return Ok(result);
         }
     }
 }
